3c3b04236126c77f0e00194ba7a35015f413697f
KaijianLi <46711007+20162430413@users.noreply.github.com>
2019-05-04 23:14:38 +0800

    后台菜品查询、添加功能实现

--- agile/src/main/java/com/order/controller/DishController.java
+++ agile/src/main/java/com/order/controller/DishController.java
@@ -24,7 +24,6 @@ public class DishController {
 
 	private final DishCategoryService dishCategoryService;
 	
-
 	@Autowired
 	public DishController(DishService dishService, DishCategoryService dishCategoryService) {
 
